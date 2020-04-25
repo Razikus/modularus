c5284698421ad97ba0e1ad1196105409659102d1
Adam Raźniewski <adam@razniewski.eu>
2020-04-25 11:02:08 +0200

    update

--- SView.ts
+++ SView.ts
@@ -5,14 +5,16 @@ export class SView {
     priority: number
     routeKey: string
     componentName: string
+    parentKey: string
     
-    constructor(icon: string, translationKey: string, routeKey: string, priority: number, privileges: Array<String>, componentName: string) {
+    constructor(icon: string, translationKey: string, routeKey: string, priority: number, privileges: Array<String>, componentName: string, parentKey: string) {
         this.icon = icon
         this.translationKey = translationKey
         this.priority = priority
         this.neededPrivileges  = privileges
         this.routeKey = routeKey
         this.componentName = componentName
+        this.parentKey = parentKey
     }
 
 }
